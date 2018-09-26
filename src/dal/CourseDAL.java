d83e4710673311529358c501ebf1cb9c5a2f8f5e
simonssonlukas <43546853+simonssonlukas@users.noreply.github.com>
2018-09-26 10:24:07 +0200

    ny hej

--- src/dal/CourseDAL.java
+++ src/dal/CourseDAL.java
@@ -1,6 +1,8 @@
 package dal;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import databaseConnetion.DBConnection;
 import model.Course;
@@ -28,7 +30,7 @@ public void runExecuteUpdate(String sqlString) throws SQLException {
 
 // Hitta en kurs och dess information
 public Course findCourse(String courseID) throws SQLException {
-	String sqlString = "select * from Course where courseID = '" + courseID + "';";
+	String sqlString = "SELECT * FROM Course WHERE courseID = '" + courseID + "';";
 	ResultSet rs = runExecuteQuery(sqlString);
 	if (rs.next()) {
 		courseID = rs.getString(1);
@@ -42,6 +44,75 @@ public Course findCourse(String courseID) throws SQLException {
 	}
 	return null;
 }
+
+//lägg till ny kurs
+public void addCourse(String courseID, String courseName, int credit) throws SQLException{
+	String sqlString = "INSERT INTO Course VALUES { '" + courseID + "', '" + courseName + "', " + credit + ");";
+	runExecuteUpdate(sqlString);
+	ps.close();
+	conn.close();
 }
+// Ta bort kurs
+public void deleteCourse(String courseID) throws SQLException {
+	String sqlString = "DELETE FROM Course WHERE courseID ='" + courseID + "';";
+	runExecuteUpdate(sqlString);
+	ps.close();
+	conn.close();
+}
+//Hämtar alla kurser
+public ArrayList<Course> getAllCourses() throws SQLException {
+	ArrayList<Course> allCourses = new ArrayList<Course>();
+	String sqlString = "SELECT * FROM Course";
+	
+	ResultSet rs = runExecuteQuery(sqlString);
+	while (rs.next()) {
+		String courseID = rs.getString(1);
+		String courseName = rs.getString(2);
+		int credit = rs.getInt(3);
+		Course c = new Course(courseID, courseName, credit);
+		allCourses.add(c);
+	}
+	ps.close();
+	conn.close();
+	return allCourses;
+}
+//Hämtar betygsfördelningen på en viss urs
+public HashMap<String, String> getGradePercentage(String courseID) throws SQLException {
+	HashMap<String, String> map = new HashMap<String, String>();
+	
+	String sqlString = "select grade, (count(grade)* 100 / (select count(*) from Studied where courseID ='" + courseID
+			+ "')) as 'percentage' from Studied where courseID = '" + courseID + "' group by grade";
+
+ResultSet rs = runExecuteQuery(sqlString);
+while (rs.next()) {
+	String grade = rs.getString(1);
+	String percent = rs.getString(2);
+	map.put(grade, percent);
+}
+ps.close();
+conn.close();
+return map;
+}
+//Hämtar top 10 högsta genomströmning
+
+public HashMap<String, String> getFlow() throws SQLException {
+	HashMap<String, String> map = new HashMap<String, String>();
+	
+	String sqlString = "select top 10 upper(courseID) as 'CourseID', (sum(case when grade != 'U' then 1 else 0 end)*100) / count (courseID) as 'Percent Passed' "
+			+ "from studied " + "group by courseID " + "order by 'Percent Passed' desc";
+
+	ResultSet rs = runExecuteQuery(sqlString);
+	while (rs.next()) {
+		String courseID = rs.getString(1);
+		String percent = rs.getString(2);
+		map.put(courseID, percent);
+
+	}
+	return map;
+}
+
+}
+
+
 
 
